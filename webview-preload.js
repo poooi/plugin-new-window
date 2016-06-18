0e92419d35df67045b95f7aaa7a56b56b8618feb
KochiyaOcean <root@kochiyaocean.org>
2016-06-18 14:40:28 +0800

    Fix wrong ref

--- webview-preload.js
+++ webview-preload.js
@@ -2,4 +2,4 @@ console.log('This is a workaround for https://bugs.chromium.org/p/chromium/issue
 var electron = require('electron')
 var remote = electron.remote
 document.onclick = (e) =>
-  remote.getCurrentWebContents().executeJavaScript("$('webview').focus()")
+  remote.getCurrentWindow().webContents.executeJavaScript("$('webview').focus()")
