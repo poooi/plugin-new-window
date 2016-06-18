1b8be7ced3c6fb90bafe4fd910fce7581d34a40e
KochiyaOcean <root@kochiyaocean.org>
2016-06-18 14:46:24 +0800

    Fix onclick event overrides

--- webview-preload.js
+++ webview-preload.js
@@ -1,5 +1,6 @@
 console.log('This is a workaround for https://bugs.chromium.org/p/chromium/issues/detail?id=600395')
 var electron = require('electron')
 var remote = electron.remote
-document.onclick = (e) =>
-  remote.getCurrentWindow().webContents.executeJavaScript("$('webview').focus()")
+document.onDOMContentLoaded = (e) =>
+  document.onclick = (e) =>
+    remote.getCurrentWindow().webContents.executeJavaScript("$('webview').focus()")
