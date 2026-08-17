fe6c04a03f099ea80ed50952cf2d45a218eb8cd2
東風谷オーシャン <root@kochiyaocean.org>
2026-08-18 03:59:47 +0900

    fix: export

--- index.ts
+++ index.ts
@@ -4,20 +4,25 @@ import os from 'os'
 
 const { config } = window
 
-// FIXME: titlebarStyle: https://github.com/electron/electron/issues/14129
-const titleBarStyle: BrowserWindowConstructorOptions['titleBarStyle'] =
-  process.platform === 'darwin' && Number(os.release().split('.')[0]) >= 17 ? 'hidden' : undefined
+// Named exports, not `export default`: poi loads a transpiled plugin with
+// `await import()`, which puts a CJS `module.exports` under `default` instead of
+// spreading it. Only named exports reach poi on both the .ts and the .js path.
 
-export default {
-  windowURL: `file://${__dirname}/index.html`,
-  windowOptions: {
-    x: config.get('poi.window.x', 0),
-    y: config.get('poi.window.y', 0),
-    width: 1100,
-    height: 700,
-    enableLargerThanScreen: true,
-    titleBarStyle,
-  },
-  realClose: true,
-  multiWindow: true,
+export const windowURL = `file://${__dirname}/index.html`
+
+export const windowOptions: BrowserWindowConstructorOptions = {
+  x: config.get('poi.window.x', 0),
+  y: config.get('poi.window.y', 0),
+  width: 1100,
+  height: 700,
+  enableLargerThanScreen: true,
+  // FIXME: titlebarStyle: https://github.com/electron/electron/issues/14129
+  titleBarStyle:
+    process.platform === 'darwin' && Number(os.release().split('.')[0]) >= 17
+      ? 'hidden'
+      : undefined,
 }
+
+export const realClose = true
+
+export const multiWindow = true
